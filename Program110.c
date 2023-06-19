3c3ae6a078ec39eb460556a23bb57883526454c8
ShubhangiShelake <136948352+ShubhangiShelake@users.noreply.github.com>
2023-06-19 19:53:25 +0530

    Add files via upload
    Problems On Pattern Printing in C

---
+++ Program110.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i=0, j=0;
+
+    if(iRow<0)
+    {
+        iRow=-iRow;
+    }
+    if(iCol<0)
+    {
+        iCol=-iCol;
+    }
+    if(iRow!=iCol)
+    {
+        printf("Row and coloum are differant:\n");
+        return;
+    }
+
+    for(i=1;i<iRow; i++)
+    {
+        for(j=1; j<iCol; j++)
+        {
+            if((i==j))
+            {
+                printf("#\t");   
+            }
+            else
+            {
+                printf("%d\t",i);
+            }
+
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter number of row:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of coloum:\n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1,iValue2);
+
+    return 0;
+}
---
+++ Program93.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+  int iCnt=0;
+  char Ch='A';
+
+  if(iNo<0)
+  {
+    iNo=-iNo;
+  }
+
+  for(iCnt=1; iCnt<iNo;iCnt++,Ch++)
+  {
+    printf("%c\t",Ch);
+  }
+ 
+  printf("\n");
+
+}
+
+int main()
+{
+   int iValue=0;
+
+   printf("Enter number:\n");
+   scanf("%d",&iValue);
+
+   Display(iValue);
+
+  return 0;
+}
